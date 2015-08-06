4523091c384f7d05d35a6788d3a96483989a3fa8
neorusa <neorusa@ya.ru>
2015-08-06 15:50:48 +0500

    My first app!

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__author__ = 'neorusa'
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == "__main__":
+    PongApp().run()
